e8828c7043b5502b3217fa91c389a1b011ba543c
saltaid <salteaid000@stu.rockwallisd.org>
2022-11-08 17:15:39 -0600

    aaaaaaauuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuugggggggggggghhhhhhhhhhhhhhhhhhh

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -2,6 +2,7 @@ from bell.avr.mqtt.client import MQTTModule
 from bell.avr import utils
 from bell.avr.utils import timing
 from bell.avr.mqtt.payloads import(
+    AvrAutonomousEnablePayload,
     AvrFcmVelocityPayload,
     AvrPcmSetBaseColorPayload,
     AvrApriltagsVisiblePayload,
@@ -14,22 +15,53 @@ from loguru import logger
 import time
 class Sandbox(MQTTModule):
     bruh = 0
+    isAutonomous = False
     def __init__(self) -> None:
         super().__init__()
         self.topic_map = {"avr/fcm/velocity": self.show_velocity}
         self.topic_map = {"avr/apriltags/visible": self.show_april_tag_detected}
-        #self.topic_map = {"avr/pcm/set_temp_color": self.show_balls}
-    def show_april_tag_detected(self, payload: AvrApriltagsVisiblePayload) -> None:
-        self.send_message(
-            "avr/pcm/set_base_color",
-            {"wrgb": (0, 0, 0, 0)}
+        self.topic_map = {"avr/autonomous/enable": self.autonomous_enabled}
+
+    def autonomous_enabled(self, payload: AvrAutonomousEnablePayload) -> None:
+        self.isAutonomous = payload["enabled"]
+        logger.debug(f'{self.isAutonomous = }')
+        if self.isAutonomous:
+            
+            time.sleep(1)
+            self.send_message(
+            "avr/pcm/set_temp_color",
+            {"wrgb": (0, random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))}
         )
+            
+            time.sleep(1)
+            self.send_message(
+            "avr/pcm/set_temp_color",
+            {"wrgb": (0, random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))}
+        )
+            time.sleep(1)
+            self.send_message(
+            "avr/pcm/set_temp_color",
+            {"wrgb": (0, random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))}
+        )
+           
+            
+            
+            
+          
+
+    def light_1(self):
+        self.flash_lights()
+    def light_2(self):
+        self.flash_lights()
+    def light_3(self):
+        self.flash_lights()
+
+    def show_april_tag_detected(self, payload: AvrApriltagsVisiblePayload) -> None:
         apriltagList = payload["tags"]
-        for _ in range(3):
-            logger.debug("before wait")
-            self.flash_lights()
-            logger.debug("After wait")
+        logger.info('April tag detected')
+        logger.info(f'tag id: {apriltagList[0]}')
         #self.open_servo(2)
+
     def show_velocity(self, payload: AvrFcmVelocityPayload) -> None:
         vx = payload["vX"]
         vy = payload["vY"]
@@ -37,18 +69,14 @@ class Sandbox(MQTTModule):
         v_ms = (vx, vy, vz)
         logger.debug(f"Velocity information: {v_ms} m/s")
 
-
-    #def show_balls(self, payload: AvrPcmSetTempColorPayload) -> None:
-     #   self.lights_on
-
     def open_servo(self, channel) -> None:
         pass
         #self.send_message(
          #   "avr/pcm/set_servo_open_close",
           #  {"servo": channel, "action": "open"}
         #)
+
     def flash_lights(self) -> None:
-        
         #for i in range(3):
         self.bruh = self.bruh + 1
         logger.debug("flash_lights()")
@@ -57,16 +85,6 @@ class Sandbox(MQTTModule):
             "avr/pcm/set_temp_color",
             {"wrgb": (0, random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))}
         )
-        
-#        self.send_message(
-#            "avr/pcm/set_temp_color",
-#            {"wrgb": (0, 0, 255, 0)}
-#        )
-#        
-#        self.send_message(
-#            "avr/pcm/set_temp_color",
-#            {"wrgb": (0, 0, 0, 255)}
-#        )
 
 if __name__ == "__main__":
     box = Sandbox()
